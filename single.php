d8428a418af85cfc326efeb0fddab5500ed75058
TommieMedia <jkeston@stthomas.edu>
2017-05-09 14:36:28 -0500

    Add files via upload

---
+++ single.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_header(); ?>
+
+<div class="wrap wsingle">
+
+	<div id="primary" class="content-area">
+		<div id="leaderboard">
+		<?php echo adrotate_group(2); ?>
+		</div><!--leaderboard-->
+		<main id="main" class="site-main" role="main">
+			<div id="main-column">
+			<div id="central-content" class="cc-single">
+			<?php
+				/* Start the Loop */
+				while ( have_posts() ) : the_post();
+
+					get_template_part( 'template-parts/post/content', get_post_format() );
+
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+
+					the_post_navigation( array(
+						'prev_text' => '<span class="screen-reader-text">' . __( 'Previous Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Previous', 'twentyseventeen' ) . '</span> <span class="nav-title"><span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '</span>%title</span>',
+						'next_text' => '<span class="screen-reader-text">' . __( 'Next Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Next', 'twentyseventeen' ) . '</span> <span class="nav-title">%title<span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ) . '</span></span>',
+					) );
+
+				endwhile; // End of the loop.
+			?>
+			</div><!--main column-->
+				<?php get_sidebar(); ?>
+			</div><!--central-content-->
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
+
+<?php get_footer();
